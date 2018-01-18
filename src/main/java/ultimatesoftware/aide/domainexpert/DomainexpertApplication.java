925055f7835ec922981a98e6e4b09dc414bfa938
Justin Phillips <euangelion3o9@gmail.com>
2018-01-18 12:10:07 -0500

    Inital code commit

---
+++ src/main/java/ultimatesoftware/aide/domainexpert/DomainexpertApplication.java
@@ -0,0 +1,12 @@
+package ultimatesoftware.aide.domainexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainexpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainexpertApplication.class, args);
+	}
+}
